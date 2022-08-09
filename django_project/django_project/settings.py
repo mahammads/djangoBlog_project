f9638c59109db45a732e5a75a888cb740d2ba61f
mahammads <106377568+mahammads@users.noreply.github.com>
2022-08-09 21:19:18 +0530

    Update settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -135,5 +135,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-EMAIL_HOST_USER = 'mahammad.sarwad@exlservice.com'
-EMAIL_HOST_PASSWORD = 'Mjaas@31193'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
